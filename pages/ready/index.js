e8be7d9d277a55d4a8a6eba2f16ca0efa49b0c8e
Jieyuhua9193 <1500690094@qq.com>
2020-08-06 08:50:33 +0800

    udpate

--- pages/ready/index.js
+++ pages/ready/index.js
@@ -1,20 +1,3 @@
 Page({
-  data: {
-    animation: {},
-    ani: null,
-  },
-  onLoad() {
-    this.ani = wx.createAnimation({
-      duration: 400,
-      timingFunction: "ease-in-out",
-    });
-  },
-  start() {
-    this.ani.scale(2, 2).rotate(34);
-    this.ani.translate(200, 100);
-    this.ani.step();
-    this.setData({
-      animation: this.ani.export(),
-    });
-  },
+
 });
